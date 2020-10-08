f4391a1af2158c65c6e7e01e0c2aaf2038c0ac93
stephen mutuku <s.muthinimutuku@gmail.com>
2020-10-08 16:48:57 +0300

    user model update

--- users/models.py
+++ users/models.py
@@ -1,9 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,UserManager,
+from django.db.models.functions import datetime,
 
-class CustomUser(AbstractUser):
-    phone=models.CharField(max_length=15,help_text='please enter a valid phone number')
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(db_index=True, unique=True)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-class BusinessRegistration:
-    models.OneToOneField(CustomUser)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', ]
+
+    objects = UserManager()
+
+    @property
+    def token(self):
+        dt = datetime.now() + timedelta(days=days)
+        token = jwt.encode({
+            'id': user_id,
+            'exp': int(time.mktime(dt.timetuple()))
+        }, settings.SECRET_KEY, algorithm='HS256')
+        return token.decode('utf-8')
+
+    def get_full_name(self):
+        return (self.first_name + ' ' + self.last_name)
+
+    def get_short_name(self):
+        return self.first_name
+
+    def natural_key(self):
+        return (self.first_name, self.last_name)
+
+    def __str__(self):
+        return self.email
